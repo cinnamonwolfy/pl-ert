9c3de7ff4fc4fa8fc6a76539cd0c26a1d7a2e00a
pocketlinux32 <pocketlinux32@gmail.com>
2023-07-01 06:22:10 -0500

    fixed some stuff. temporary build system is still broken

--- compile.sh
+++ compile.sh
@@ -4,9 +4,9 @@
 set -e
 mkdir obj -p
 cd obj
-clang -I../include -fno-stack-protector -nostartfiles -nodefaultlibs -g -c ../src/asm/pl-syscall-x86_64.s ../src/posix/*
-ar rc ../libpl-ert.a *.o
+clang -DIS64BIT -I../include -fno-stack-protector -nostartfiles -nodefaultlibs -g -c ../src/asm/pl-syscall-x86_64.s ../src/posix/*
+ar rc ../libpl-ert.a pl-syscall-x86_64.o open.o read.o write.o exit.o
 cd ..
-clang -Iinclude -static -g -L. -lpl-ert pl-ert-test.c -o pl-ert-test.out
+clang -Iinclude -static -fno-stack-protector -nostartfiles -nodefaultlibs -g -L. -lpl-ert pl-ert-test.c -o pl-ert-test.out
 
 ./pl-ert-test.out
