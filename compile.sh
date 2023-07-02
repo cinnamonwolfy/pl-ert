0a9c16d3c4806e3b6dfb135b76bb5e056409fa70
pocketlinux32 <pocketlinux32@gmail.com>
2023-07-02 00:12:58 -0500

    possibly pl-ert 0.01 release :3

--- compile.sh
+++ compile.sh
@@ -4,9 +4,9 @@
 set -e
 mkdir obj -p
 cd obj
-clang -DIS64BIT -I../include -fno-stack-protector -nostartfiles -nodefaultlibs -g -c ../src/asm/pl-syscall-x86_64.s ../src/posix/*
+clang -v -DIS64BIT -I../include -ffreestanding -fno-stack-protector -nostdinc -nodefaultlibs -g -c ../src/asm/pl-syscall-x86_64.s ../src/posix/*
 ar rc ../libpl-ert.a pl-syscall-x86_64.o open.o read.o write.o exit.o
 cd ..
-clang -Iinclude -static -fno-stack-protector -nostartfiles -nodefaultlibs -g -L. -lpl-ert pl-ert-test.c -o pl-ert-test.out
+clang -v -Iinclude -static -ffreestanding -fno-stack-protector -nostdinc -nostartfiles -nodefaultlibs -g -L. pl-ert-test.c -lpl-ert -o pl-ert-test.out
 
 ./pl-ert-test.out
