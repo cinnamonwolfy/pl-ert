1a90b7939d764be6dbdcac198c98059b64295ecf
pocketlinux32 <pocketlinux32@gmail.com>
2023-07-17 03:45:43 -0500

    i thought i had pushed this

--- include/pl-unistd.h
+++ include/pl-unistd.h
@@ -42,10 +42,37 @@
 #define S_ISGID 1024
 #define S_ISVTX 512
 
-extern int pl_syscall(int syscall, int paramNum, void* params);
+/* List of mmap() flags and modes */
+#define PROT_EXEC 4
+#define PROT_READ 1
+#define PROT_WRITE 2
+#define PROT_NONE 0
+#define MAP_SHARED 1
+#define MAP_SHARED_VALIDATE 3
+#define MAP_PRIVATE 2
+#define MAP_32BIT 64
+#define MAP_ANON 32
+#define MAP_ANONYMOUS 32
+#define MAP_DENYWRITE 2048
+#define MAP_EXECUTABLE 4096
+#define MAP_FILE 0
+#define MAP_FIXED 16
+#define MAP_FIXED_NOREPLACE 1048576
+#define MAP_GROWSDOWN 256
+#define MAP_HUGETLB 262144
+#define MAP_LOCKED 8192
+#define MAP_NONBLOCK 65536
+#define MAP_NORESERVE 16384
+#define MAP_POPULATE 32768
+#define MAP_STACK 131072
+#define MAP_SYNC 524288
+
+extern long pl_syscall(int syscall, int paramNum, void* params);
 
 int open(const char* pathname, int flags, int mode);
 int close(int fd);
 int read(int fd, void* buffer, unsigned int length);
 int write(int fd, void* data, unsigned int length);
 void exit(int code);
+void* mmap(void* addr, unsigned long length, int prot, int flags, int fd, long offset){
+int munmap(void* addr, unsigned long length);
