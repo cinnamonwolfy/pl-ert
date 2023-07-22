4e47d1c3b655d229dc970d201135f29e22ba7491
pocketlinux32 <pocketlinux32@gmail.com>
2023-07-22 02:52:21 -0500

    forgor to rename lseek to lseek

--- src/posix/mmap.c
+++ src/posix/mmap.c
@@ -2,5 +2,5 @@
 
 void* mmap(void* addr, unsigned long length, int prot, int flags, int fd, long offset){
 	void* ptr[6] = { addr, (void*)length, (void*)prot, (void*)flags, (void*)fd, (void*)offset };
-	return pl_syscall(__NR_mmap, 6, ptr);
+	return (void*)pl_syscall(__NR_mmap, 6, ptr);
 }
